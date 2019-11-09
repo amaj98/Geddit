8dc538b14f792d0830998e51d870d7f989d7990c
xxxG4T0Rxxx <nickmcd33@gmail.com>
2019-11-09 13:42:13 -0500

    firebase database connected

---
+++ public/index.js
@@ -0,0 +1,26 @@
+var ready = ()=>{
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+        // // The Firebase SDK is initialized and available here!
+        //
+        // firebase.auth().onAuthStateChanged(user => { });
+        // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+        // firebase.messaging().requestPermission().then(() => { });
+        // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+        //
+        // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+        
+        let app = firebase.app();
+        var db = firebase.database();
+        var ref = db.ref("geddittest");
+        ref.once("value",function(snapshot){
+            console.log(snapshot.val());
+        });
+
+        var subRef = ref.child("subgeddit");
+        subRef.set({
+            name: "testsubreddit",
+            posts:"postid1,postid2"
+        });
+            
+}
