9e686d9abb00604f643165e07c457b8662a184f4
xxxG4T0Rxxx <nickmcd33@gmail.com>
2019-11-14 18:22:15 -0500

    Added Oauth for google acct

--- public/index.js
+++ public/index.js
@@ -1,20 +1,75 @@
 var ready = ()=>{
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    //
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-        // // The Firebase SDK is initialized and available here!
-        //
-        // firebase.auth().onAuthStateChanged(user => { });
-        // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-        // firebase.messaging().requestPermission().then(() => { });
-        // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-        //
-        // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 
-        
-        let app = firebase.app();
-        var db = firebase.database();
-        var ref = db.ref("geddittest");
-        ref.once("value",function(snapshot){
-            console.log(JSON.stringify(snapshot.val()));
-        });
+    
+    let app = firebase.app();
+    var db = firebase.database();
+    var ref = db.ref("geddittest");
+    ref.once("value",function(snapshot){
+        console.log(JSON.stringify(snapshot.val()));
+    });
+    var myid = "";
+
+
+var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        myid = user["uid"];
+        console.log("User :" + user["uid"]);
+        $("#authstuff").html(`<h1>Welcome ${user.displayName}</h1><p><img src="${user.photoURL}"></p>`);
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+    });
+
+    $("#join").click(function(){ //CREATE
+        var name = $("#myname").val();
+
+        if (!!name){
+            db.ref().child("people/"+myid).set({name: name, online: true});
+        }
+    });
+
+    $("#leave").click(function(){ //DESTROY
+        db.ref("people/"+myid).remove();
+    });
+
+    $("#edit").click(function(){ //UPDATE
+        var name = $("#myname").val();
+
+        if (!!name){
+            db.ref("people").child(myid).update({name: name});
+        }
+    });
+
+
+
+    db.ref("people").on("value", function(snap){
+        $("#people").empty();
+        var everyone = snap.val();
+
+        for(var id in everyone){
+            if (everyone.hasOwnProperty(id)){
+            let yourname = everyone[id].name || "anonymous";
+            $("#people").append(`<li>${yourname}</li>`);
+            }
+        }
+    });
             
 }
