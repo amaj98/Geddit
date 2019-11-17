1aa20d3e42987e1946a39641499e816ccbcea525
xxxG4T0Rxxx <nickmcd33@gmail.com>
2019-11-16 20:15:07 -0500

    Added sick top button

--- public/index.js
+++ public/index.js
@@ -66,6 +66,21 @@ var readyAll = ()=>{
         });
     }
     $("#userbutton").click(() => googleLogin());
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("scrollButton").style.display = "block";
+        } else {
+            document.getElementById("scrollButton").style.display = "none";
+        }
+    }
+
+    $("#scrollButton").click(() => {
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
 }
 
 var readyHome = () => {
