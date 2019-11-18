7f3f79e577fcbc25c8a55ab12148d5f10cd0279e
xxxG4T0Rxxx <nickmcd33@gmail.com>
2019-11-17 22:02:06 -0500

    Added posts, comments, fixed upvotes and downvotes

--- public/index.js
+++ public/index.js
@@ -65,9 +65,8 @@ var readyAll = ()=>{
     }
     $("#userbutton").click(() => googleLogin());
 
-    // When the user scrolls down 20px from the top of the document, show the button
+    // Scroll Button Stuff //
     window.onscroll = function() {scrollFunction()};
-
     function scrollFunction() {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             document.getElementById("scrollButton").style.display = "block";
@@ -75,13 +74,13 @@ var readyAll = ()=>{
             document.getElementById("scrollButton").style.display = "none";
         }
     }
-
     $("#scrollButton").click(() => {
         //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
         document.body.scrollTop = 0; // For Safari
     });
 }
 
+// HOME PAGE //
 var readyHome = () => {
     $("#createGoGeddit").click(() => firebase.auth().currentUser ? $("#goGedditStuffStuff").toggle() : googleLogin());
 
@@ -91,6 +90,8 @@ var readyHome = () => {
         if (top && desc) {
             db.ref().child("goGeddits/" + top).set({ desc: desc, owner: myid });
             $("#goGedditStuffStuff").toggle();
+            //document.location.reload();
+            window.location.replace(window.location.href+"/go/"+top);
         }
         else {
             alert("Please fill in all fields.")
@@ -98,13 +99,13 @@ var readyHome = () => {
 
     });
 
+    var deleteGoGeddit = ()=> {
+        db.ref().child("goGeddits").once('value', (snapshot) => snapshot.forEach((child) => {
 
-    // FUNCTIONS //
-    var createGoGeddit = (TOPIC, DESCRIPTION) => {
-        db.ref().child("goGeddits/" + TOPIC).push({ name: TOPIC, desc: DESCRIPTION, owner: myid });
-        document.getElementById("goGedditStuffStuff").style.display = 'none';
+        }));
     }
 
+    // 
     db.ref().child("goGeddits").once('value', (snapshot) => snapshot.forEach((child) => {
         db.ref('gedders/' + child.val().owner).once('value', (snapshot) => {
             $(".gogeddits").append(`<div class="card text-center">
@@ -112,6 +113,7 @@ var readyHome = () => {
             <h5 class="card-title">${child.key}</h5>
             <p class="card-text">${child.val().desc}</p>
             <a href="/go/${child.key}" class="btn btn-primary">Go</a>
+            <b id="deleteButton" class="btn btn-danger" > Delete </b>
             </div>
             <div class="card-footer text-muted">
             created by ${snapshot.val().name}
@@ -133,6 +135,7 @@ var readyGo = () => {
         if(title && bod){
             goref.child("posts/" + title).set({creator: myid, body:bod});
             $(".createPost").toggle();
+            document.location.reload();
         }
         else alert("Invalid Post");
     });
@@ -142,7 +145,7 @@ var readyGo = () => {
             <div class="card-body">
             <h5 class="card-title">${child.key}</h5>
             <p class="card-text">${child.val().body}</p>
-            <a href="/go/${gogeddit}/${child.key}" class="btn btn-primary">Go</a>
+            <a href="/go/${gogeddit}/post/${child.key}" class="btn btn-primary">Go</a>
             <p></p>
             <button type = "button" class = "btn btn-success" id = "U${child.key}">GoUp</button>
             <button type = "button" class = "btn btn-danger" id = "D${child.key}">GoDown</button>
@@ -167,7 +170,7 @@ var readyGo = () => {
     
             $("#U"+child.key).click(()=>{
                 goref.child("posts/"+child.key+"/voters/"+myid).once("value",(snapshot)=>{
-                    let v = snapshot.val();
+                    let v = snapshot.val()?snapshot.val():0;
                     switch(v){
                         case -1:
                             goref.child("posts/"+child.key+"/voters/"+myid).set(1);
@@ -187,7 +190,7 @@ var readyGo = () => {
 
             $("#D"+child.key).click(()=>{
                 goref.child("posts/"+child.key+"/voters/"+myid).once("value",(snapshot)=>{
-                    let v = snapshot.val();
+                    let v = snapshot.val()?snapshot.val():0;
                     switch(v){
                         case -1:
                             goref.child("posts/"+child.key+"/voters/"+myid).set(0);
@@ -199,6 +202,103 @@ var readyGo = () => {
                             break;
                         case 1:
                             goref.child("posts/"+child.key+"/voters/"+myid).set(-1);
+                            $("#NV"+child.key).text(parseInt($("#NV"+child.key).text()) - 2);
+                            break;    
+                    }
+                })
+            });
+
+
+
+        });
+    }));
+}
+
+var readyPost = () => {
+    var post = decodeURI(window.location.href.split('/').pop());
+    var gogeddit = decodeURI(window.location.href.split('/')[4]);
+    let postref = db.ref("goGeddits/"+gogeddit+"/posts/"+post);
+    $(".header").prepend(post);
+    $("#commentbutton").click(()=>$(".createComment").toggle());
+    $('#submitComment').click(()=>{
+        let bod = $('#commentBody').val()
+        if(bod){
+            postref.child("comments").push({body:bod,commentor: myid});
+            $(".createComment").toggle();
+            document.location.reload();
+        }
+        else alert("Invalid Comment");
+    });
+    postref.once('value',(snapshot)=>$(".post").append(`
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">${snapshot.key}</h1>
+    <p class="lead">${snapshot.val().body}</p>
+  </div>
+</div>`));
+    
+    postref.child("comments").once('value', (snapshot) => snapshot.forEach((child) => {
+        db.ref('gedders/' + child.val().commentor).once('value', (snapshot) => {
+            $(".comments").append(`<div class="card text-center">
+            <div class="card-body">
+            <p class="card-text">${child.val().body}</p>
+            <p></p>
+            <button type = "button" class = "btn btn-success" id = "U${child.key}">GoUp</button>
+            <button type = "button" class = "btn btn-danger" id = "D${child.key}">GoDown</button>
+            <p></p>
+            <div class="badge badge-pill badge-dark" style = "font-size:16px;" id = "NV${child.key}">0</div>
+            </div>
+            <div class="card-footer text-muted">
+            post by ${snapshot.val().name}
+            </div>
+        </div>`
+            )
+            //"#U"+key is the id for upvote button for post with that key
+            //D is downvote button
+            //NV is number of votes
+
+            //loop through voters for post and adds up vals, then sets sum in html
+            
+            postref.child("comments/"+child.key+"/voters").once("value",(snapshot)=>
+                snapshot.forEach((voter)=>{$("#NV"+child.key).text(parseInt($("#NV"+child.key).text()) + voter.val());})
+            );
+            
+            
+            $("#U"+child.key).click(()=>{
+                console.log(child.key);
+                postref.child("comments/"+child.key+"/voters/"+myid).once("value",(snapshot)=>{
+                    let v = snapshot.val()?snapshot.val():0;
+                    switch(v){
+                        case -1:
+                            postref.child("comments/"+child.key+"/voters/"+myid).set(1);
+                            $("#NV"+child.key).text(parseInt($("#NV"+child.key).text()) + 2);
+                            break;
+                        case 0:
+                            postref.child("comments/"+child.key+"/voters/"+myid).set(1);
+                            $("#NV"+child.key).text(parseInt($("#NV"+child.key).text()) + 1);
+                            break;
+                        case 1:
+                            postref.child("comments/"+child.key+"/voters/"+myid).set(0);
+                            $("#NV"+child.key).text(parseInt($("#NV"+child.key).text()) - 1);
+                            break;    
+                    }
+                })
+            });
+
+            $("#D"+child.key).click(()=>{
+                postref.child("comments/"+child.key+"/voters/"+myid).once("value",(snapshot)=>{
+                    let v = snapshot.val()?snapshot.val():0;
+                    switch(v){
+                        case -1:
+                            postref.child("comments/"+child.key+"/voters/"+myid).set(0);
+                            $("#NV"+child.key).text(parseInt($("#NV"+child.key).text()) + 1);
+                            break;
+                        case 0:
+                            postref.child("comments/"+child.key+"/voters/"+myid).set(-1);
+                            $("#NV"+child.key).text(parseInt($("#NV"+child.key).text()) - 1);
+                            break;
+                        case 1:
+                            postref.child("comments/"+child.key+"/voters/"+myid).set(-1);
                             $("#NV"+child.key).text(parseInt($("#NV"+child.key).text()) - 2);
                             break;    
                     }
